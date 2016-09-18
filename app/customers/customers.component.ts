a2364e98995aeef6d21a302b7d26c62db0a3db88
Tarun Sharma <tarun.softengg@gmail.com>
2016-09-19 00:14:32 +0530

    js map generation set to false

--- app/customers/customers.component.ts
+++ app/customers/customers.component.ts
@@ -1,35 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+    import { Component, OnInit } from '@angular/core';
 
-import { DataService } from '../shared/services/data.service';
-import { ICustomer } from '../shared/interfaces';
+    import { DataService } from '../shared/services/data.service';
+    import { ICustomer } from '../shared/interfaces';
 
-@Component({
-    moduleId: module.id,
-    selector: 'customers',
-    templateUrl: 'customers.component.html'
-})
-export class CustomersComponent implements OnInit {
-    
-    customers: ICustomer[] = [];
-    editId: number = 0;
-    errorMessage: string;
+    @Component({
+        moduleId: module.id,
+        selector: 'customers',
+        templateUrl: 'customers.component.html'
+    })
+    export class CustomersComponent implements OnInit {
+        
+        customers: ICustomer[] = [];
+        editId: number = 0;
+        errorMessage: string;
 
-    constructor(private dataService: DataService) {  }
+        constructor(private dataService: DataService) {  } 
+        ngOnInit() { 
+            this.dataService.getCustomersSummary()
+                .subscribe((data: ICustomer[]) => this.customers = data);
+        }
+        save(customer: ICustomer) {
+            this.dataService.updateCustomer(customer)
+                .subscribe((status: boolean) => {
+                    if (status) {
+                        this.editId = 0;
+                    } else {
+                        this.errorMessage = 'Unable to save customer';
+                    }
+                })
+        }
+        getCustomers(customer: ICustomer) {
+             this.dataService.getCustomer()
+                .subscribe((data: ICustomer[]) => this.customers = data);
+      } 
 
-    ngOnInit() { 
-        this.dataService.getCustomersSummary()
-            .subscribe((data: ICustomer[]) => this.customers = data);
     }
-    
-    save(customer: ICustomer) {
-        this.dataService.updateCustomer(customer)
-            .subscribe((status: boolean) => {
-                if (status) {
-                    this.editId = 0;
-                } else {
-                    this.errorMessage = 'Unable to save customer';
-                }
-            })
-    }
-
-}
---
+++ app/technology/CourseComponent.js
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-container',
+    template: `<h1>LIst of courses</h1>`
+})
+export class CourseComponent {
+
+}
